5ac4182be419af15d20b74af03b035fa51ece790
DentVega <dentvega6@gmail.com>
2020-01-21 00:19:30 -0400

    - Recibir notificacion - onMessage, onLaunch y onResume

--- lib/src/providers/push_notifications_provider.dart
+++ lib/src/providers/push_notifications_provider.dart
@@ -10,5 +10,19 @@ class PushNotificationProvider {
       print(token);
       // fVyFUCCs49Q:APA91bE5ZVpWjZoRxJOC2coc1FPt_mgQ-QEL-KOinLMr6NG4brjx6jtXr_AQXGrWdiK_QyObXJBjL4T9y1r67njAKWSXFtQI9VxH05Z4zg2p2G63sDer0s9IVGD_ZwNQIFdyENL1OJSD
     });
+    _firebaseMessaging.configure(onMessage: (info) async {
+      print('=== On Message ===');
+      print(info);
+    }, onLaunch: (info) async {
+      print('=== On Launch ===');
+      print(info);
+      final noti = info['data']['comida'];
+      print(noti);
+    }, onResume: (info) async {
+      print('=== On Resume ===');
+      print(info);
+      final noti = info['data']['comida'];
+      print(noti);
+    });
   }
 }
